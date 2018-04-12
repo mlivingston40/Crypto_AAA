3f87780712c6cef1e050f1eeb827681a59a60059
mjlivingston40 <matt.j.livingston.40@gmail.com>
2018-04-12 13:40:20 -0400

    momentum and volume classes done

--- Math/momentum.py
+++ Math/momentum.py
@@ -1,17 +1,35 @@
+import pandas as pd
+
 class Momentum:
 
-    def __init__(self, num_coins_allocate, df, start_date, end_date):
+    def __init__(self, rel_universe, num_coins_allocate, df, start_date, end_date):
 
+        self.rel_universe = rel_universe
         self.num_coins = num_coins_allocate
-        # This will already be in memory - the master dataframe from class Data #
-        self.data = df
         # This should be the start int corresponding in the trade_days dataframe that is in memory #
         self.start_date_int = start_date
         self.end_date_int = end_date
+        # This will already be in memory - the master dataframe from class Data #
+        self.df_mom = df[(df.date >= self.start_date_int) & (df.date <= self.end_date_int)]
 
-    def relevant_coin_list(self):
-        # This will turn the relevant coins to buy for each iteration through the trade_days dataframe #
-        # Which will then be used to feed the analytics in the results.py module #
-        pass
+    def top_momentum_df(self):
+
+        returns = []
+        for i in self.rel_universe:
+
+            mom = (self.df_mom[self.df_mom.Pair == i].weightedAverage.tail(1).values - self.df_mom[
+                self.df_mom.Pair == i].weightedAverage.head(1).values) / self.df_mom[self.df_mom.Pair == i].weightedAverage.head(
+                1).values
+
+            returns.append({'Pair': i, 'Momentum': mom[0]})
+
+        returns = pd.DataFrame(returns)
+
+        top_returns = returns.sort_values(by=['Momentum'], ascending = False).head(self.num_coins)
+
+        #account for only positive momentum #
+
+        return top_returns[top_returns.Momentum > 0]
+
 
 
--- Math/volume.py
+++ Math/volume.py
@@ -1,15 +1,33 @@
+import pandas as pd
+
 class Volume:
 
     def __init__(self, coin_filter, df, start_date, end_date):
 
         self.universe_coins_num = coin_filter
-        # This will already be in memory - the master dataframe from class Data #
-        self.data = df
         # This should be the start int corresponding in the trade_days dataframe that is in memory #
         self.start_date_int = start_date
         self.end_date_int = end_date
+        # This will already be in memory - the master dataframe from class Data #
+        self.df_vol = df[(df.date >= self.start_date_int) & (df.date <= self.end_date_int)]
 
     def relevant_universe_list(self):
         # This will turn the relevant universe for each iteration through the trade_days dataframe #
         # Which will then be used to feed the analytics in the results.py module #
-        pass
+
+        pairs = self.df_vol.groupby('Pair').agg({'volume': 'count'})
+        # Probably want to bake in a filter for pairs that aren't there for the whole past volume_window_days_ #
+
+        coin_volume = []
+        for i in pairs.index:
+            data = self.df_vol[self.df_vol['Pair'] == i]
+            avg = data.volume.sum() / len(data)
+            coin_volume.append({'coin': i, 'avg_vol': avg})
+        coin_volume = pd.DataFrame(coin_volume)
+
+        # sort for top based upon 'universe_amt' parameter and return the list #
+
+        return list(coin_volume.sort_values(by=['avg_vol'], ascending=False).head(self.universe_coins_num))
+
+
+
